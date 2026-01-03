c2b207acbeec0fb76f7ec43ed91aadc25bfa4642
Flo908 <florentin908@gmail.com>
2026-01-03 03:05:03 +0100

    Did some things
    Next:
    - Split large components into smaller, reusable components
    - Improved file organization for clarity and maintainability
    - General code optimizations and cleanup

---
+++ firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQViQpKUwCDTtiOCEkqT-vhOcppumJ-_I",
+  authDomain: "loanly-b111e.firebaseapp.com",
+  projectId: "loanly-b111e",
+  storageBucket: "loanly-b111e.firebasestorage.app",
+  messagingSenderId: "757076041004",
+  appId: "1:757076041004:web:323eb053462dcde4b0a089",
+  measurementId: "G-SVPVN14ZV7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
